de70fcd8eb43bd78b4212b4487cb78b0a388987e
Johnny Leung <eternaldragonx@hotmail.com>
2021-08-10 17:29:08 -0400

    Add files via upload

---
+++ src/components/index.js
@@ -0,0 +1,14 @@
+import Bar from '@/components/Charts/Bar'
+import ChartCard from '@/components/Charts/ChartCard'
+import MiniProgress from '@/components/Charts/MiniProgress'
+import MiniArea from '@/components/Charts/MiniArea'
+
+import Info from '@/components/Info/Info'
+
+export {
+    Bar,
+    ChartCard,
+    MiniProgress,
+    MiniArea,
+    Info,
+}
